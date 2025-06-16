05ea83a56fb76738f02e3003c41f1297440a2633
imrazzaditya <imrazzaditya@gmail.com>
2025-06-16 13:49:02 +0530

    initial commit

---
+++ basic1.js
@@ -0,0 +1,42 @@
+window.console.log("Hello");
+
+//console.log(age);
+//var age =20; //declaration & initialiazing
+//console.log(age);
+
+//var age = 30;  //re-declaration
+//console.log(age);
+
+let age = 20;
+console.log(age);
+console.log(1+1);
+console.log('1'+1);
+console.log(1+'1');
+console.log('1'+'1');
+console.log(1-1);
+console.log('a'- 1);
+console.log(1 -'a');
+console.log('1'-'a');
+console.log(1*1);
+console.log('a'* 1);
+console.log(1 * 'a');
+console.log('a'*'1');
+console.log(1/1);
+console.log('a'/1);
+console.log(1/'a');
+console.log('1'/'a');
+
+
+
+//Type Coersion implicit
+
+console.log('10'- 2);
+console.log(20 - '5');
+console.log(20 - '5');
+console.log('50' - '10');
+console.log(10*2);
+console.log(20*'5');
+console.log('50'*'10');
+console.log('10'/2);
+console.log(20/'5');
+console.log('50'/'10');
